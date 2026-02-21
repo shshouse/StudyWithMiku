9b0ecc9c7906acefe51a210789434a0743034d20
yeewater <182350104+yeewater@users.noreply.github.com>
2026-02-21 11:14:15 +0800

    Clean up updates.js by removing old entries
    Removed multiple outdated update entries and retained the latest update.

--- src/data/updates.js
+++ src/data/updates.js
@@ -1,83 +1,11 @@
 export const updates = [
-  {  
-  date: '2026-2-17',
-  content: [
-    'B站的Rainash_1在视频下留言了苹果端在未播放音乐时计时结束铃声不播放的bug',
-    '感谢反馈！由于主包没有苹果设备，只能猜测大概原因来尝试修复，可以试试现在正常了吗',
-    '如果大家有遇到问题也可以在视频下留言，主包高强度刷B的',
-    '或者发邮件到shshouse@mikumod.com，主包都会回复的！',
-    '歌单也是投到这个邮箱＞﹏＜',
-  ]
-  },
   {
-  date: '2026-2-14',
+  date: '2026-2-17',
   content: [
     'fork自用，夹带点私货',
     '源项目：https://github.com/shshouse/StudyWithMiku'
   ]
-  },
-  {
-  date: '2026-2-13',
-  content: [
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-  ]
-},
-{
-  date: '2026-1-18',
-  content: [
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-    'awa',
-  ]
-},
-{
-  date: '2026-1-4',
-  content: [
-    'awa',
-  ]
-},
-{
-  date: '2026-1-2',
-  content: [
-    'awa',
-  ]
-},
-{
-  date: '2025-12-27',
-  content: [
-    'awa',
-  ]
-},
-{
-  date: '2025-12-19',
-  content: [
-    'qwq',
-  ]
-}
+  }
 ]
 
 export const LATEST_UPDATE_VERSION = updates[0].date
