699d24be1e97373c12f0f439e39564b2903f87d4
shshouse <1839192968@qq.com>
2026-05-15 12:09:43 +0800

    SEO

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+if (window.location.pathname!=='/'&&!window.location.pathname.includes('.')){
+  window.location.replace('/')//自动回根！
+}
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
