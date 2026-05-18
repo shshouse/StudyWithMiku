6fdaaa31a9338b03f0a39a7b0931f07b50d6ed68
shshouse <1839192968@qq.com>
2026-05-18 22:43:17 +0800

    更新推荐歌单，修正描述并移除冗余ID

--- src/data/playlists.js
+++ src/data/playlists.js
@@ -1,14 +1,12 @@
 export const recommendPlaylists = [
   {
-    id: '1',
     name: '站长神秘歌单+8首神秘音乐',
-    desc: '?',
+    desc: '♦',
     platform: 'netease',
     playlistId: '8894040639',
     tags: ['神秘','有Miku','较慢','平静']
   },
   {
-    id: '2',
     name: '【学习向】Galgame纯音乐',
     desc: '静听一首gal曲子，品剧中故事。',
     platform: 'netease',
@@ -16,7 +14,6 @@ export const recommendPlaylists = [
     tags: ['Gal','治愈','by-千禧2k-']
   },
   {
-    id: '3',
     name: '学习（真）歌单',
     desc: '好耶ヾ(✿ﾟ▽ﾟ)ノ',
     platform: 'netease',
@@ -24,7 +21,6 @@ export const recommendPlaylists = [
     tags: [ '轻音乐','平静','by 炤华夕逝']
   },
   {
-    id: '4',
     name: '古典歌单',
     desc: '希望可以让更多人了解古典音乐',
     platform: 'tencent',
@@ -32,7 +28,6 @@ export const recommendPlaylists = [
     tags: ['古典', '纯音乐','By 阿氏圆']
   },
   {
-    id: '5',
     name: 'ACG睡眠曲目',
     desc: '让我们回忆一下，纯音乐的味道，静下心，慢慢合上双眼，享受吧！',
     platform: 'netease',
@@ -40,7 +35,6 @@ export const recommendPlaylists = [
     tags: ['轻音乐','ACG','By 永生']
   },
   {
-    id: '6',
     name: '东方 舒缓纯音',
     desc: '世间桃园，唯幻想乡',
     platform: 'netease',
@@ -48,12 +42,11 @@ export const recommendPlaylists = [
     tags: ['轻音乐','白噪音','By 永生']
   },
   {
-    id: '7',
-    name: '白噪音歌单',
-    desc: '自然白噪音背景，适合学习',
+    name: '酸の心',
+    desc: '♦',
     platform: 'netease',
-    playlistId: '17889326270',
-    tags: ['轻音乐','白噪音','By 松灰酸']
+    playlistId: '5438089118',
+    tags: ['Kawaii','Snails house','True Music']
   },
 ]
 
